a1d37f0b5d3d7f46a55660f3318a9c8468a563b3
Zoooora <1205294218@qq.com>
2021-11-24 20:14:37 +0800

    2021/11/23 教师和学生的登录
    2021/11/24
    美化登陆页面，优化登陆错误信息

--- src/main/java/com/database/dbdesign/model/loginModel.java
+++ src/main/java/com/database/dbdesign/model/loginModel.java
@@ -1,7 +1,7 @@
 package com.database.dbdesign.model;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 import org.springframework.ui.Model;
@@ -18,14 +18,14 @@ public class loginModel {
     public String loginAction(String loginType, String loginId, String loginPassword, HttpSession session, Model model){
         String sql = loginType.equals("student") ? studentLogin() : teacherLogin();
         if(StringUtils.hasText(loginId) && StringUtils.hasText(loginPassword)){
-            String clientId = jdbcTemplate.queryForObject(sql, String.class,
-                    loginId, loginPassword);
-            if(StringUtils.hasText(clientId)){
+            try{
+                String clientId = jdbcTemplate.queryForObject(sql, String.class,
+                        loginId, loginPassword);
                 session.setAttribute("loginClientId", clientId);
+                session.setAttribute("loginClientType", loginType);
                 return loginType.equals("student") ?
-                        "redirect:/studentMain.html" : "redirect:/teacherMain.html";
-            }
-            else{
+                            "redirect:/studentMain.html" : "redirect:/teacherMain.html";
+            }catch (EmptyResultDataAccessException e){
                 model.addAttribute("msg", "账号密码错误");
                 return "login";
             }
@@ -43,4 +43,6 @@ public class loginModel {
     private String teacherLogin(){
         return "select Tno from teacher where Tno = ? and Tpassword = ?";
     }
+
+
 }
